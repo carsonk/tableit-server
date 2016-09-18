f516017e797efb686dfab6c0f0a13783629ac0bd
Carson Killbreath <kill4992@kettering.edu>
2016-09-18 03:06:12 -0400

    Got an index listing orders.

--- kitchen/views.py
+++ kitchen/views.py
@@ -4,9 +4,28 @@ from django.http import JsonResponse
 from .models import MenuItem, Order
 from django.views.decorators.csrf import csrf_exempt
 import json
+import datetime
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    page = "<html><head><title>Recent Orders</title></head><body>"
+
+    recent_orders = Order.objects.all().order_by('-time_submitted')
+
+    for order in recent_orders:
+        total_price = 0
+        print(order)
+        page += "<div><em>Ordered %s</em><ol>" % order.time_submitted.isoformat()
+
+        menu_items = order.menu_items.all()
+        for item in menu_items:
+            page += "<li>%s - $%d</li>" % (item.title, item.price)
+            total_price += item.price
+
+        page += "</ol>Total: <strong>$%d</strong></div><br />" % total_price
+
+    page += "</body></html>"
+
+    return HttpResponse(page)
 
 def menu(request):
     menu = list(MenuItem.objects.all().values())
