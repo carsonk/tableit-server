77a510639d8f8c4e572261494e505c0d1b54f508
Carson Killbreath <kill4992@kettering.edu>
2016-09-19 19:19:27 +0000

    Added __str__ options for models.'

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+import datetime
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.SmallIntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
 
 class Order(models.Model):
     time_submitted = models.DateTimeField(auto_now_add=True)
     menu_items = models.ManyToManyField(MenuItem)
+    
+    def __str__(self):
+        return self.time_submitted.isoformat()
+
